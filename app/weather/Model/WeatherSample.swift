8bfa753532d711e11fb11e410c06f7d34d3ba886
Richard Dancsi <wimagguc@gmail.com>
2016-10-24 02:13:51 +0200

    [update] weather sample model

---
+++ app/weather/Model/WeatherSample.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherSample.swift
+//  weather
+//
+//  Created by Richard Dancsi on 24/10/2016.
+//  Copyright © 2016 wimagguc. All rights reserved.
+//
+
+import Foundation
+
+class WeatherSample {
+    
+    var timestamp: Int?
+    var temperature: Float?
+    var minTemperature: Float?
+    var maxTemperature: Float?
+    var weatherMain: String?
+    var weatherDescription: String?
+    
+}
